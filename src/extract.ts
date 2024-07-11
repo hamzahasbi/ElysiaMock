7d8716c759d04a3175d6ce424c2f188e2e58384a
Hamza Hasbi <hamza.hasbi@gmail.com>
2024-07-11 10:09:09 +0100

    Bulk commit

---
+++ src/extract.ts
@@ -0,0 +1,21 @@
+import { readdir } from "node:fs/promises";
+
+export const prepare = async () => {
+  const path = "./data";
+  const files = await readdir(path, { recursive: true });
+  let endpoints : Endpoints = {};
+  for (const file of files)  {
+    const input = Bun.file(`${path}/${file}`);
+
+    const uri = file.replaceAll('-', '/').replaceAll('_', '/:').replaceAll('$', '_').replaceAll(".json", "");
+    const content = await input.json();
+    console.log(uri);
+    endpoints[uri] = content;
+  }
+  return endpoints;
+}
+
+
+interface Endpoints {
+  [uri: string]: any;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
 import { Elysia } from "elysia";
+const endpoints = await require('./extract').prepare();
+const app = new Elysia();
+app.onError(({error }) => {
+  return new Response(error.toString())
+}).get("/*", ({ params }) => {
+  const uri =  params['*'];
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+  if (Object.hasOwn(endpoints, uri)) {
+    return endpoints[uri];
+  }
+  throw new Error('Server is during maintenance')
+}).post("/*", ({ params }) => {
+  const uri =  params['*'];
+
+  if (Object.hasOwn(endpoints, uri)) {
+    return endpoints[uri];
+  }
+  throw new Error('Server is during maintenance')
+}).put("/*", ({ params }) => {
+  const uri =  params['*'];
+
+  if (Object.hasOwn(endpoints, uri)) {
+    return endpoints[uri];
+  }
+  throw new Error('Server is during maintenance')
+}).listen(9000);
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
